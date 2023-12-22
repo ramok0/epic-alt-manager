95f8b303d4ef5f78686f62553fac7a42363ca4d1
unknown <63268168+ramok0@users.noreply.github.com>
2023-12-22 16:09:24 +0100

    chore(windows.rs) : format

--- src/gui/window.rs
+++ src/gui/window.rs
@@ -1,26 +1,29 @@
 use std::sync::Arc;
 
 use egui_toast::Toast;
-use tokio::sync::{mpsc::Sender, Mutex, mpsc::Receiver};
+use tokio::sync::{ mpsc::Sender, Mutex, mpsc::Receiver };
 
-use super::windows::{add_account::AddAccountWindow, clone_configuration::{CloneControlsData, CloneControlsWindow}, settings::RuntimeSettings};
+use super::windows::{
+    add_account::AddAccountWindow,
+    clone_configuration::{ CloneControlsData, CloneControlsWindow },
+    settings::RuntimeSettings,
+};
 
 #[derive(PartialEq, Clone, Hash, Eq, Debug)]
 pub enum EWindow {
     AddAccount,
     CloneSettings(CloneControlsData),
-    Settings
+    Settings,
 }
 
 #[derive(Clone)]
 pub struct WindowDescriptor {
-    pub kind:EWindow,
-    pub runtime_settings:Arc<std::sync::Mutex<RuntimeSettings>>
+    pub kind: EWindow,
+    pub runtime_settings: Arc<std::sync::Mutex<RuntimeSettings>>,
 }
 
-
 pub struct WindowManager {
-    pub current_window:Option<(EWindow, Box<dyn SubWindow>)>,
+    pub current_window: Option<(EWindow, Box<dyn SubWindow>)>,
 }
 
 impl WindowManager {
@@ -28,7 +31,7 @@ impl WindowManager {
         Self { current_window: None }
     }
 
-    pub fn render(&mut self, ctx:&egui::Context, ui:&mut egui::Ui) {
+    pub fn render(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
         if self.current_window.is_some() {
             let window = &mut self.current_window.as_mut().unwrap().1;
             if window.should_appear() {
@@ -37,34 +40,45 @@ impl WindowManager {
         }
     }
 
-    pub fn set_window(&mut self, window:WindowDescriptor, shared_data:WindowSharedData) {
+    pub fn set_window(&mut self, window: WindowDescriptor, shared_data: WindowSharedData) {
         match window.kind.clone() {
             EWindow::AddAccount => {
-                self.current_window = Some((window.kind.clone(), Box::new(AddAccountWindow::new(shared_data, window.clone()))));
-            },
+                self.current_window = Some((
+                    window.kind.clone(),
+                    Box::new(AddAccountWindow::new(shared_data, window.clone())),
+                ));
+            }
             EWindow::CloneSettings(_info) => {
-                self.current_window = Some((window.kind.clone(), Box::new(CloneControlsWindow::new(shared_data, window.clone()))));
-            },
+                self.current_window = Some((
+                    window.kind.clone(),
+                    Box::new(CloneControlsWindow::new(shared_data, window.clone())),
+                ));
+            }
             EWindow::Settings => {
-                self.current_window = Some((window.kind.clone(), Box::new(super::windows::settings::SettingsWindow::new(shared_data, window.clone()))));
+                self.current_window = Some((
+                    window.kind.clone(),
+                    Box::new(
+                        super::windows::settings::SettingsWindow::new(shared_data, window.clone())
+                    ),
+                ));
             }
         }
     }
 }
 
 pub trait SubWindow {
-    fn new(shared_data:WindowSharedData, window_descriptor:WindowDescriptor) -> Self where Self: Sized;
-    fn create_window<'a>(&self, ui:&egui::Ui) -> egui::Window<'a> where Self:Sized;
-    fn render(&mut self, ctx:&egui::Context, ui:&mut egui::Ui);
+    fn new(shared_data: WindowSharedData, window_descriptor: WindowDescriptor) -> Self
+        where Self: Sized;
+    fn create_window<'a>(&self, ui: &egui::Ui) -> egui::Window<'a> where Self: Sized;
+    fn render(&mut self, ctx: &egui::Context, ui: &mut egui::Ui);
     fn close(&mut self);
     fn should_appear(&self) -> bool;
 }
 
-
 pub enum EventKind {
     Accounts(Vec<String>),
     AddToast(Toast),
-    CurrentAccount(Option<String>)
+    CurrentAccount(Option<String>),
 }
 
 pub type EventSender = Sender<EventKind>;
@@ -73,6 +87,6 @@ pub type EventManager = (EventSender, EventReceiver);
 
 pub struct WindowSharedData {
     pub configuration: Arc<Mutex<crate::config::Configuration>>,
-    pub accounts:Vec<String>,
-    pub event_sender:EventSender
+    pub accounts: Vec<String>,
+    pub event_sender: EventSender,
 }
